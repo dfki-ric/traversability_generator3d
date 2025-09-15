2da6861edd21a77e2b7b74727437bd1afa668259
haider8645 <haider_lodhi@hotmail.com>
2025-09-15 07:06:44 +0200

    Remove autogenerated file form git track

--- source_dependencies/env.sh
+++
@@ -1,5 +0,0 @@
-export CMAKE_PREFIX_PATH=/opt/workspace/planning/traversability_generator3d/build/install/share/rock/cmake/:$CMAKE_PREFIX_PATH
-export PKG_CONFIG_PATH=/opt/workspace/planning/traversability_generator3d/build/install/lib/pkgconfig/:$PKG_CONFIG_PATH
-export LD_LIBRARY_PATH=/opt/workspace/planning/traversability_generator3d/build/install/lib:/opt/workspace/planning/traversability_generator3d/build/install/lib64:$LD_LIBRARY_PATH
-export PATH=/opt/workspace/planning/traversability_generator3d/build/install/bin:$PATH
-export VIZKIT_PLUGIN_RUBY_PATH=/opt/workspace/planning/traversability_generator3d/build/install/lib
