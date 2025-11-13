88e3b758b33e137fb3164222855b9a00ff695f40
Christoph Hertzberg <christoph.hertzberg@dfki.de>
2025-11-13 16:04:41 +0100

    Add missing osdependency

--- source_dependencies/install_os_dependencies.bash
+++ source_dependencies/install_os_dependencies.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-DEPS="doxygen libeigen3-dev libcgal-dev libpcl-dev libboost-filesystem-dev libboost-serialization-dev libboost-system-dev"
+DEPS="doxygen libeigen3-dev libcgal-dev libpcl-dev libboost-filesystem-dev libboost-serialization-dev libboost-system-dev libopenscenegraph-dev"
 
 if dpkg --verify $DEPS; then
   echo "All OS dependencies are installed already. To update them run:"
